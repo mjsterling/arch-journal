e90c14dbd3b65a1d7f8d93d370f6563c7e348015
Matthew Sterling <mjsterling93@gmail.com>
2025-04-19 00:21:48 +1000

    fix linter errors #6

--- src/app/data/GlobalStateProvider.tsx
+++ src/app/data/GlobalStateProvider.tsx
@@ -181,7 +181,7 @@ export default function GlobalStateProvider({
   const [materialStorage, setMaterialStorage] = useState<{
     [P in Materials]: number;
   }>(() => {
-    const materialStorageState = localStorage.getItem(
+    const materialStorageState = window.localStorage.getItem(
       'arch-journal-materialStorage'
     );
     if (materialStorageState) {
@@ -195,7 +195,7 @@ export default function GlobalStateProvider({
       ...prev,
       [material]: amount,
     }));
-    localStorage.setItem(
+    window.localStorage.setItem(
       'arch-journal-materialStorage',
       JSON.stringify({
         ...materialStorage,
