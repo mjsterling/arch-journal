0158c1c1e0196534e51cc0f6ef3685edfbeabb73
mjsterling <mjsterling93@gmail.com>
2025-09-15 19:41:25 +1000

    fixed styling on damaged mat display

--- src/components/DamagedMatsNeeded.tsx
+++ src/components/DamagedMatsNeeded.tsx
@@ -42,7 +42,7 @@ export function DamagedMatsNeeded({ mode = 'artefacts' }: { mode: 'artefacts' | 
     return _materialsNeeded;
   }, [artefacts, mode]);
   return (
-    <div>
+    <div className="flex flex-col gap-4 md:grid md:grid-cols-[repeat(auto-fit,minmax(200px,1fr))]">
       {Object.entries(materialsNeeded)
         .filter(([, amount]) => amount > 0)
         .sort(([name1], [name2]) => name1.localeCompare(name2))
